a02b6d85f6c45e9355d93a438d169145ff41b831
Fabien Zucchet <fabien.zucchet@viarezo.fr>
2023-03-09 13:28:03 +0100

    temp commit

--- src/cmd/dataPatchEmissions.ts
+++ src/cmd/dataPatchEmissions.ts
@@ -1,86 +1,57 @@
 import ProgressBar from 'progress';
 import config from '../config';
+import emissionsCoef from '../data/emissions/coefficients.json';
 import { PoolClient } from 'pg';
 
-const batchSize = 10000;
-
 async function run(): Promise<void> {
-    const pool = await config.pg();
+  const pool = await config.pg();
 
-    const client = await pool.connect();
-    try {
-        // Fetch all product hashes
-        config.logger.info('Fetching all product hashes');
-        const productHashes = await getProductHashes(client);
+  const client = await pool.connect();
 
-        const progressBar = new ProgressBar(
-            '-> loading [:bar] :percent (:etas remaining)',
-            {
-              width: 40,
-              complete: '=',
-              incomplete: ' ',
-              renderThrottle: 500,
-              total: productHashes.length,
-            }
-          );
-
-        // For each product hash, insert by batch the emissions data into the product table
-        config.logger.info('Updating product emissions');
-        for (let i = 0; i < productHashes.length; i += batchSize) {
-            const batch = productHashes.slice(i, i + batchSize);
-
-            await client.query('BEGIN');
-
-            await Promise.all(batch.map((productHash) => {
-                updateProductEmissions(client, productHash);
-                progressBar.tick();
-            }));
-
-            await client.query('COMMIT');
-        }
-
-    } catch (e) {
-        await client.query('ROLLBACK');
-
-        throw e;
+  const progressBar = new ProgressBar(
+    '-> loading [:bar] :percent (:etas remaining)',
+    {
+      width: 40,
+      complete: '=',
+      incomplete: ' ',
+      renderThrottle: 500,
+      total: Object.keys(emissionsCoef).length,
     }
+  );
 
+  // For each product hash, insert by batch the emissions data into the product table
+  config.logger.info('Updating product emissions');
+
+  for (const [key, coefficient] of Object.entries(emissionsCoef)) {
+    const [_, skuId, region] = key.split('_');
+    await updateProductEmissions(client, skuId, region, coefficient);
+    progressBar.tick();
+  }
 }
 
-async function getProductHashes(client: PoolClient): Promise<string[]> {
-    const result = await client.query(
-        `
-        SELECT "productHash"
-        FROM "products"
-        `
-    );
+const updateProductEmissions = async (
+  client: PoolClient,
+  skuId: string,
+  region: string,
+  coefficient: number
+): Promise<void> => {
+  const emissionData = JSON.stringify([
+    {
+      emissionHash: 'emissionHash',
+      unit: 'kgCO2e',
+      emissions: coefficient,
+      startUsageAmount: 0,
+    },
+  ]);
 
-    return result.rows.map((row) => row.productHash);
-}
-
-async function updateProductEmissions(client: PoolClient, productHash: string): Promise<void> {
-    const emissionsData = generateEmissionsData();
-    await client.query(
-        `
-        UPDATE "products"
-        SET "emissions" = $1
-        WHERE "productHash" = $2
-        `,
-        [emissionsData, productHash]
-    );
-}
-
-const generateEmissionsData = () => {
-    return JSON.stringify([{
-        emissionHash: 'SampleEmissionHash',
-        unit: 'kgCO2e',
-        emissions: Math.random() * 100,
-        effectiveDateStart: '2020-01-01',
-        effectiveDateEnd: '2024-12-31',
-        startUsageAmount: 0,
-        endUsageAmount: 100,
-        description: 'Sample Description',
-    }]);
+  await client.query(
+    `
+      UPDATE "products"
+      SET "emissions" = $1
+      WHERE "sku" = $2 AND "region" = $3
+      `,
+    [emissionData, skuId, region]
+  );
 };
 
 config.logger.info('Starting: loading data into DB');
